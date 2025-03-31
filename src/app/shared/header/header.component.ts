1d3aa6ac5d39689b92f9850027ae84baa6463dac
Ghazaleh razi <ghazale.razi@gmail.com>
2025-03-31 17:48:01 +0330

    refactor: simplify header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,17 +1,15 @@
-import { Component, Signal, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgIf],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  private themeService = inject(ThemeService);
-  theme: Signal<'light' | 'dark'> = this.themeService.currentTheme;
+  themeService = inject(ThemeService);
+  theme = this.themeService.currentTheme;
 
   toggleTheme() {
     this.themeService.toggleTheme();
